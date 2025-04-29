9bdb1e4b7cfb32cea4e1e25e17be972364f7bc7c
leuzaoo <leuzaoocontato@gmail.com>
2025-04-29 20:08:24 -0300

    Created category section and a toggle button to switch between sweeties and savory pastries

---
+++ src/components/ui/ToggleSwitch.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+interface ToggleSwitchProps {
+  options: string[];
+  value: string;
+  onChange: (option: string) => void;
+}
+
+const ToggleSwitch: React.FC<ToggleSwitchProps> = ({
+  options,
+  value,
+  onChange,
+}) => {
+  const handleToggle = (option: string) => {
+    onChange(option);
+  };
+
+  return (
+    <div className="flex gap-3">
+      {options.map((option) => (
+        <button
+          key={option}
+          className={`cursor-pointer rounded-[20px] px-5 py-3 shadow-sm transition-all duration-500 ease-in-out md:text-2xl ${
+            value === option ? "bg-dark text-light" : "text-dark bg-white"
+          }`}
+          onClick={() => handleToggle(option)}
+          type="button"
+        >
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default ToggleSwitch;
