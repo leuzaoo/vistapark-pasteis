7243ea3072b7f3295e0c701287cac204563d5fd7
leuzaoo <leuzaoocontato@gmail.com>
2025-04-28 21:13:20 -0300

    Added links to navigate through the app

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useState } from "react";
+import Link from "next/link";
 
 import { productList, ProductItem } from "@/data/products";
 import { useCart } from "@/contexts/CartContext";
@@ -12,7 +13,7 @@ export default function Home() {
 
   return (
     <div className="max-w-2xl mx-auto p-4">
-      <div className="flex justify-center space-x-4 mb-6">
+      <div className="flex justify-center space-x-4 mb-6 items-center">
         <button
           onClick={() => setShowSweet(true)}
           className={`px-4 py-2 rounded ${
@@ -29,6 +30,9 @@ export default function Home() {
         >
           Salgados
         </button>
+        <Link href="/checkout" className="text-white px-4 py-2 bg-red-500">
+          Carrinho
+        </Link>
       </div>
 
       <div className="grid grid-cols-2 gap-4">
