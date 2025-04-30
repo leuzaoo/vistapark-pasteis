97bd0add1ee1c380baa5a8f9017743560662f13d
leuzaoo <leuzaoocontato@gmail.com>
2025-04-30 10:40:54 -0300

    Created the add and remove quantity button

---
+++ src/components/ui/AddRemoveButton.tsx
@@ -0,0 +1,41 @@
+import { MinusIcon, PlusIcon } from "lucide-react";
+import React from "react";
+
+interface Props {
+  increase: () => void;
+  decrease: () => void;
+  disabled: boolean;
+  quantity: number;
+  isZero: boolean;
+}
+
+const AddRemoveButton = ({
+  decrease,
+  increase,
+  disabled,
+  quantity,
+  isZero,
+}: Props) => {
+  return (
+    <div className="flex items-center justify-center">
+      <button
+        onClick={decrease}
+        disabled={disabled}
+        className={`bg-dark/15 cursor-pointer rounded-l-full p-3 transition-all duration-500 ease-in-out ${isZero ? "opacity-30" : "hover:bg-dark/30"}`}
+      >
+        <MinusIcon size={20} />
+      </button>
+      <span className="bg-dark/5 px-4 py-2 text-lg font-medium">
+        {quantity}
+      </span>
+      <button
+        onClick={increase}
+        className="bg-dark/15 hover:bg-dark/30 cursor-pointer rounded-r-full p-3 transition-all duration-500 ease-in-out"
+      >
+        <PlusIcon size={20} />
+      </button>
+    </div>
+  );
+};
+
+export default AddRemoveButton;
