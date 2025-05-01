e2de23a89b2cf65a96bf9d4c5d34e99bc74f46e5
leuzaoo <leuzaoocontato@gmail.com>
2025-05-01 15:27:22 -0300

    Created PriceInfosSectionRight component to render all product carts in large screens

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { useCart, CartItem } from "@/contexts/CartContext";
 import { productList } from "@/data/products";
 
+import PriceInfosSectionRight from "@/components/ui/PriceInfosSectionRight";
 import PriceInfosSection from "@/components/ui/PriceInfosSection";
 import EmptyCartLayout from "@/components/pages/EmptyCartLayout";
 import FoodCartCard from "@/components/ui/FoodCartCard";
@@ -17,37 +18,40 @@ const CartPage = () => {
   }
 
   return (
-    <div className="relative mx-auto h-screen max-w-3xl px-4 py-8 md:px-0">
+    <div className="relative mx-auto h-screen max-w-4xl px-4 py-4">
       <HeaderCart href="/menu" />
 
-      <div className="h-[465px] overflow-y-auto pr-3">
-        {cart.map((item: CartItem) => {
-          const product = productList.find((p) => p.id === item.id);
-          if (!product) return null;
+      <div className="sm:grid sm:grid-cols-2 sm:gap-5">
+        <div className="h-[520px] overflow-y-auto pr-3 pb-8 sm:h-[600px] sm:pb-0">
+          {cart.map((item: CartItem) => {
+            const product = productList.find((p) => p.id === item.id);
+            if (!product) return null;
 
-          return (
-            <FoodCartCard
-              key={item.id}
-              item={item}
-              product={product}
-              length={item.qty}
-              onAdd={() =>
-                addItem({
-                  id: item.id,
-                  name: item.name,
-                  qty: 1,
-                  image: item.image,
-                  price: item.price,
-                })
-              }
-              onDecrease={() => removeQty(item.id, 1)}
-              onDeleteItem={() => removeItem(item.id)}
-              onUpdateNotes={(notes) => updateNotes(item.id, notes)}
-            />
-          );
-        })}
+            return (
+              <FoodCartCard
+                key={item.id}
+                item={item}
+                product={product}
+                length={item.qty}
+                onAdd={() =>
+                  addItem({
+                    id: item.id,
+                    name: item.name,
+                    qty: 1,
+                    image: item.image,
+                    price: item.price,
+                  })
+                }
+                onDecrease={() => removeQty(item.id, 1)}
+                onDeleteItem={() => removeItem(item.id)}
+                onUpdateNotes={(notes) => updateNotes(item.id, notes)}
+              />
+            );
+          })}
+        </div>
+        <PriceInfosSection />
+        <PriceInfosSectionRight />
       </div>
-      <PriceInfosSection />
     </div>
   );
 };
---
+++ src/components/ui/PriceInfosSectionRight.tsx
@@ -0,0 +1,69 @@
+import { ChevronRightIcon } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+
+import { formatCurrency } from "@/utils/formatCurrency";
+import { useCart, type CartItem } from "@/contexts/CartContext";
+import { productList } from "@/data/products";
+
+export default function PriceInfosSectionRight() {
+  const { cart, totalItems } = useCart();
+
+  const totalPrice = cart.reduce((sum, item) => {
+    const product = productList.find((p) => p.id === item.id);
+    if (!product) return sum;
+    return sum + product.price * item.qty;
+  }, 0);
+
+  return (
+    <div className="hidden h-full flex-col justify-between rounded-xl bg-white p-2 shadow-sm sm:flex">
+      <div className="flex h-full flex-col justify-between">
+        <ul className="space-y-2">
+          {cart.map((item: CartItem) => {
+            const product = productList.find((p) => p.id === item.id);
+            if (!product) return null;
+
+            const itemsFinalPrice = item.price * item.qty;
+
+            return (
+              <>
+                <li>
+                  <div className="flex items-end justify-between">
+                    <h2 className="font-unbounded text-sm font-light">
+                      {item.name}
+                    </h2>
+                    <div className="font-unbounded flex items-center gap-1 text-xs">
+                      <span className="text-dark/60">{item.qty}x</span>
+                      <span className="font-bold">
+                        {formatCurrency(itemsFinalPrice)}
+                      </span>
+                    </div>
+                  </div>
+                </li>
+              </>
+            );
+          })}
+        </ul>
+        <div>
+          <div className="flex w-full items-center justify-between">
+            <span>Quantidade</span>
+            <span className="font-unbounded font-bold">x{totalItems}</span>
+          </div>
+          <div className="mt-2 flex w-full items-center justify-between">
+            <span>Valor total</span>
+            <span className="font-unbounded font-bold">
+              {formatCurrency(totalPrice)}
+            </span>
+          </div>
+          <Link
+            href="/checkout"
+            className="text-light relative mx-auto mt-3 flex w-full items-center justify-center rounded-lg bg-green-600 py-2 text-xl"
+          >
+            Finalizar
+            <ChevronRightIcon className="absolute right-4" />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
