1c87f2d51a1a92651ce539e8361a8fbeb30468a1
leuzaoo <leuzaoocontato@gmail.com>
2025-05-01 01:39:02 -0300

    Updated cart page layout style

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -4,44 +4,50 @@ import React from "react";
 import { useCart, CartItem } from "@/contexts/CartContext";
 import { productList } from "@/data/products";
 
+import PriceInfosSection from "@/components/ui/PriceInfosSection";
 import EmptyCartLayout from "@/components/pages/EmptyCartLayout";
-import CartItemCard from "@/components/ui/FoodCartCard";
+import FoodCartCard from "@/components/ui/FoodCartCard";
 import HeaderCart from "@/components/ui/HeaderCart";
 
 const CartPage = () => {
-  const { cart, addItem, removeQty, updateNotes } = useCart();
+  const { cart, addItem, removeQty, updateNotes, removeItem } = useCart();
 
   if (cart.length === 0) {
     return <EmptyCartLayout />;
   }
 
   return (
-    <div className="mx-auto max-w-lg space-y-6 p-8">
-      <HeaderCart />
+    <div className="relative mx-auto h-screen max-w-3xl px-4 py-8 md:px-0">
+      <HeaderCart href="/menu" />
 
-      {cart.map((item: CartItem) => {
-        const product = productList.find((p) => p.id === item.id);
-        if (!product) return null;
+      <div className="h-[465px] overflow-y-auto pr-3">
+        {cart.map((item: CartItem) => {
+          const product = productList.find((p) => p.id === item.id);
+          if (!product) return null;
 
-        return (
-          <CartItemCard
-            key={item.id}
-            item={item}
-            product={product}
-            onAdd={() =>
-              addItem({
-                id: item.id,
-                name: item.name,
-                qty: 1,
-                image: item.image,
-                price: item.price,
-              })
-            }
-            onDecrease={() => removeQty(item.id, 1)}
-            onUpdateNotes={(notes) => updateNotes(item.id, notes)}
-          />
-        );
-      })}
+          return (
+            <FoodCartCard
+              key={item.id}
+              item={item}
+              product={product}
+              length={item.qty}
+              onAdd={() =>
+                addItem({
+                  id: item.id,
+                  name: item.name,
+                  qty: 1,
+                  image: item.image,
+                  price: item.price,
+                })
+              }
+              onDecrease={() => removeQty(item.id, 1)}
+              onDeleteItem={() => removeItem(item.id)}
+              onUpdateNotes={(notes) => updateNotes(item.id, notes)}
+            />
+          );
+        })}
+      </div>
+      <PriceInfosSection />
     </div>
   );
 };
